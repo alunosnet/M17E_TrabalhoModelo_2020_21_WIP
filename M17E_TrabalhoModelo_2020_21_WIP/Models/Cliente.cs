c4dbeb7359373b1536d546a950bd3a432f1ebc76
Paulo Ferreira <alunosnet@gmail.com>
2021-04-06 09:17:32 +0100

    Model cliente

--- M17E_TrabalhoModelo_2020_21_WIP/Models/Cliente.cs
+++ M17E_TrabalhoModelo_2020_21_WIP/Models/Cliente.cs
@@ -17,9 +17,30 @@ namespace M17E_TrabalhoModelo_2020_21_WIP.Models
         [UIHint("Insira o nome, pelo menos 3 letras")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Tem de indicar a morada do cliente")]
+        [StringLength(110)]
+        [MinLength(3, ErrorMessage = "Morada muito pequena. Deve ter pelo menos 3 letras")]
+        [UIHint("Insira a morada do cliente, pelo menos 3 letras")]
         public string Morada { get; set; }
+
+        [Required(ErrorMessage = "Tem de indicar o código postal do cliente")]
+        [StringLength(8)]
+        [MinLength(8, ErrorMessage = "O código postal tem de ter 8 números")]
+        [Display(Name ="Código Postal")]
         public string CP { get; set; }
+
+        [Required(ErrorMessage = "Tem de indicar o email do cliente")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [StringLength(15)]
+        [MinLength(9, ErrorMessage = "Deve ter no mínimo 9 digitos")]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "Tem de a data de nascimento do cliente")]
+        [Display(Name = "Data de nascimento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
         public DateTime DataNascimento { get; set; }
         //TODO: lista das estadias
 
